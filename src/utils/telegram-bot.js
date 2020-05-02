f6738c61b0162ccdfe9ecca915a141204b3190c6
roman <68932dh_!aT>
2020-05-02 13:16:54 +0300

    Updated flow of creating telegram bot

--- src/utils/telegram-bot.js
+++ src/utils/telegram-bot.js
@@ -1,8 +1,23 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
 const vars = require('../../const/vars');
 
+let BOT;
+
+/**
+ * Method for getting instance of telegram bot
+ * @return {TelegramBot}
+ */
+
+function getBot () {
+  if (!BOT) {
+    BOT = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
+  }
+
+  return BOT;
+}
+
 module.exports.sendMsg = async (chatId, message) => {
+  const bot = getBot();
   await bot.sendMessage(process.env.APP_ENV === 'develop' ? vars.supportTelegtamChat : chatId, message);
 };
